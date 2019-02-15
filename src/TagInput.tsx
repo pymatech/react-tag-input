216ea0e39a9b24ee8c5080c846f882de4871cbce
robford123 <robford123@hotmail.com>
2019-02-15 10:16:50 -0500

    Add flex-wrap to dynamically increase height of component

--- src/TagInput.tsx
+++ src/TagInput.tsx
@@ -1,17 +1,17 @@
 import React, { KeyboardEvent } from "react"
 
-export class TagInput extends React.Component<{ className?:string, placeholder?:string },{ items:string[], input:string, focused:boolean}> {
+export class TagInput extends React.Component<{ className?: string, placeholder?: string }, { items: string[], input: string, focused: boolean }> {
   container: any;
-  inputControl:any;
-  focusTimeoutId:any = undefined;
+  inputControl: any;
+  focusTimeoutId: any = undefined;
 
-  constructor(props:any) {
+  constructor(props: any) {
     super(props);
 
     this.state = {
       items: [],
       input: "",
-      focused:false
+      focused: false
     };
 
     this.container = React.createRef();
@@ -39,64 +39,67 @@ export class TagInput extends React.Component<{ className?:string, placeholder?:
         onClick={this.grabFocus}
         onBlur={this.handleBlur}
       >
+        <div className="d-flex align-content-start flex-wrap tag-input-container">
         {this.state.items.map((item, i) => (
-          <div className="btn-group" role="group" aria-label={item}>
+          <div key={i} className="btn-group" role="group" aria-label={item}>
             <button type="button" tabIndex={0} key={i} className="btn btn-secondary tag-input-item">
-              {item}            
+              {item}
             </button>
-            <button type="button" tabIndex={0} className="btn btn-secondary mr-1 p-0 pl-1 pr-1" aria-label="Close" onClick={this.handleRemoveItem(i)}>
+            <button type="button" tabIndex={0} className="btn btn-secondary mr-1 p-0 pl-1 pr-1 tag-input-item" aria-label="Close" onClick={this.handleRemoveItem(i)}>
               <span aria-hidden="true">&times;</span>
             </button>
-          </div>            
+          </div>
         ))}
         <input
-          placeholder={ this.state.items.length===0 ? (this.props.placeholder || "") : ""}
-          ref={inputControl=> (this.inputControl = inputControl)}
+          placeholder={this.state.items.length === 0 ? (this.props.placeholder || "") : ""}
+          ref={inputControl => (this.inputControl = inputControl)}
           className="tag-input-editor"
           value={this.state.input}
           onChange={this.handleInputChange}
           onKeyDown={this.handleInputKeyDown}
         />
+        </div>
       </div>
     );
   }
 
-  handleInputChange(evt:React.ChangeEvent<HTMLInputElement>) {
+  handleInputChange(evt: React.ChangeEvent<HTMLInputElement>) {
     this.setState({ input: evt.target.value });
   }
 
   handleBlur() {
+
+    // focus tracking from https://medium.com/@jessebeach/dealing-with-focus-and-blur-in-a-composite-widget-in-react-90d3c3b49a9b
+
     this.focusTimeoutId = setTimeout(() => {
       if (this.state.focused) {
-        this.setState({ focused:false });
+        this.setState({ focused: false });
       }
     }, 0);
   }
 
   grabFocus() {
     clearTimeout(this.focusTimeoutId);
-    
-    if (!this.state.focused)
-    {
-      this.setState({focused:true});
+
+    if (!this.state.focused) {
+      this.setState({ focused: true });
       this.inputControl.focus();
     }
   }
 
-  handleFocus(){
+  handleFocus() {
     clearTimeout(this.focusTimeoutId);
-    
-    if (!this.state.focused)
-    {
-      this.setState({focused:true});
+
+    if (!this.state.focused) {
+      this.setState({ focused: true });
       this.inputControl.focus();
     }
   }
 
-  handleInputKeyDown(evt:React.KeyboardEvent<HTMLInputElement>) {
+  handleInputKeyDown(evt: React.KeyboardEvent<HTMLInputElement>) {
     if (evt.keyCode === 13) {
-      let inputControl : HTMLInputElement = evt.target as HTMLInputElement;
-      const value:string = inputControl.value;
+      let inputControl: HTMLInputElement = evt.target as HTMLInputElement;
+      const value: string = inputControl.value;
 
       this.setState(state => ({
         items: [...state.items, value],
@@ -115,12 +118,12 @@ export class TagInput extends React.Component<{ className?:string, placeholder?:
     }
   }
 
-  handleRemoveItem(index:number) {
+  handleRemoveItem(index: number) {
     return () => {
       this.setState(state => ({
         items: state.items.filter((item, i) => i !== index)
       }));
-      
+
       this.inputControl.focus();
     };
   }
